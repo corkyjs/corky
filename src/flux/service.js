aadc26b4b22ed3ca1cfe3e95f4854b8dd181b6e8
vinkobedek <vinko.bedek@acceleratio.hr>
2016-10-13 09:37:35 +0200

    service registration changes

--- src/flux/service.js
+++ src/flux/service.js
@@ -1,59 +1,49 @@
 "use strict";
-var Service = (function () {
+
+var corkyServiceSubscriptions=[];
+
+var Service = function () {
     function Service() {
         this.actions = {};
         this.isContainer = false;
+        this.isSubscribed=false;
+        this.subscriptions=[];
     }
-    Service.prototype.init = function () {
-        if (this.selector !== undefined) {
-            var data = this.selector(this.store.getState());
+    // Service.prototype.init = function () {
+       
+    //     var actions = this.actions;
+    //     var selector = this.selector;
+    //     var store = this.store;
+    //     var isContainer = this.isContainer;
+    // };
+     Service.prototype.ensureSubscription = function (store) {
+          if (this.selector !== undefined) {
+            this.selectorData = this.selector(store.getState());
         }
-        var actions = this.actions;
-        var selector = this.selector;
-        var store = this.store;
-        var isContainer = this.isContainer;
-
-        this.on('mount', function () {
-            var _this = this;
-            if (data !== undefined) {
-                Object.keys(data).forEach(function (key) {
-                    _this[key] = data[key];
+         if(!this.isSubscribed)
+         {
+             var _this=this;
+              if(corkyServiceSubscriptions.length == 0)
+             {
+                store.subscribe(function () {
+                    for(var i=0; i<corkyServiceSubscriptions.length;i++)
+                    {
+                        corkyServiceSubscriptions[i]();
+                    }
                 });
-            }
-            if (actions !== undefined) {
-                Object.keys(actions).forEach(function (actionName) {
-                    _this[actionName] = function () {
-                        var args = [];
-                        for (var _i = 0; _i < arguments.length; _i++) {
-                            args[_i - 0] = arguments[_i];
-                        }
-                        if (window.Event && window.Event.prototype.isPrototypeOf(args[0])) {
-                            args[0].preventUpdate = true;
-                        }
-                        return store.dispatch(actions[actionName].apply(actions, args));
-                    };
-                });
-            }
-            _this.on('update', function () {
-                if (selector !== undefined) {
-                    var data = selector(store.getState());
-                }
-                if (data !== undefined) {
-                    Object.keys(data).forEach(function (key) {
-                        _this[key] = data[key];
-                        if (typeof data[key] == 'function')
-                            _this.update();
-                    });
-                }
-                if (isContainer) _this.update();
-            });
+             }
+          
+             corkyServiceSubscriptions.push(function () {
+                   if (_this.selector !== undefined) {
+                       _this.selectorData =  _this.selector(store.getState());
+                   }                
+             });
+            
+             this.isSubscribed=true;
+         }
+     }
 
-            store.subscribe(function () {
-                if (isContainer) _this.update();
-            });
-            _this.update();
-        });
-    };
+       
     return Service;
-} ());
+}();
 exports.Service = Service;
--- src/tags/template.js
+++ src/tags/template.js
@@ -1,26 +1,81 @@
-import * as Riot from 'riot';
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.registerService = registerService;
+exports.registerElement = registerElement;
+
+var _riot = require("riot");
+
+var Riot = _interopRequireWildcard(_riot);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 function extend(d, element) {
-    var map = Object.keys(element.prototype).reduce((descriptors, key) => {
+    var map = Object.keys(element.prototype).reduce(function (descriptors, key) {
         descriptors[key] = Object.getOwnPropertyDescriptor(element.prototype, key);
         return descriptors;
     }, {});
     Object.defineProperties(d, map);
 }
 
-
-export function registerService(element, service) {
+function registerService(element, service) {
     element.prototype.load = function () {
-        this.mixin(service);
-    }
+       // this.mixin(service);
+
+        
+        var actions = service.actions;
+        var selector = service.selector;
+        var store = this.store;
+        var isContainer = service.isContainer;
+        service.ensureSubscription(store);
+         var _this = this;
+        if(service.isContainer){
+            store.subscribe( function(){
+                _this.update();
+            });
+        }
+        this.on('mount', function () {           
+            if (service.selectorData !== undefined) {
+                Object.keys(service.selectorData).forEach(function (key) {
+                    _this[key] = service.selectorData[key];
+                });
+            }
+            if (actions !== undefined) {
+                Object.keys(actions).forEach(function (actionName) {
+                    _this[actionName] = function () {
+                        var args = [];
+                        for (var _i = 0; _i < arguments.length; _i++) {
+                            args[_i - 0] = arguments[_i];
+                        }
+                        if (window.Event && window.Event.prototype.isPrototypeOf(args[0])) {
+                            args[0].preventUpdate = true;
+                        }
+                        return store.dispatch(actions[actionName].apply(actions, args));
+                    };
+                });
+            }
+            _this.on('update', function () {                
+                if (service.selectorData !== undefined) {
+                    Object.keys(service.selectorData).forEach(function (key) {
+                        _this[key] = service.selectorData[key];
+                        if (typeof service.selectorData[key] == 'function') _this.update();
+                    });
+                }
+                if (isContainer) _this.update();
+            });
+            _this.update();
+        });
+    };
 }
 
-export function registerElement(element, template, precompiledTags) {
+function registerElement(element, template, precompiledTags) {
 
     element.prototype["template"] = template;
     function registerTag(compiledTag) {
 
-        var transformFunction = function (opts) {
+        var transformFunction = function transformFunction(opts) {
             extend(this, element);
             element.apply(this, [opts]);
 
@@ -30,7 +85,6 @@ export function registerElement(element, template, precompiledTags) {
             if (element.prototype.beforeUnmounted !== undefined) this.on("before-unmount", this.beforeUnmounted);
             if (element.prototype.updating !== undefined) this.on("update", this.updating);
             if (element.prototype.updated !== undefined) this.on("updated", this.updated);
-
         };
 
         Riot.tag2(compiledTag.tagName, compiledTag.html, compiledTag.css, compiledTag.attribs, transformFunction, Riot.settings.brackets);
@@ -41,7 +95,7 @@ export function registerElement(element, template, precompiledTags) {
     var compiled;
 
     if (element.prototype.template !== undefined) {
-        let tagTemplate = element.prototype.template;
+        var tagTemplate = element.prototype.template;
         if (precompiledTags[tagTemplate] !== undefined) {
             compiled = precompiledTags[tagTemplate];
         } else {
