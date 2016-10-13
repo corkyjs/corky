8501fa042acf076d8e162812af097c43bdc72573
vinkobedek <vinko.bedek@acceleratio.hr>
2016-10-13 09:58:08 +0200

    rollback

--- src/tags/template.js
+++ src/tags/template.js
@@ -1,81 +1,26 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-exports.registerService = registerService;
-exports.registerElement = registerElement;
-
-var _riot = require("riot");
-
-var Riot = _interopRequireWildcard(_riot);
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+import * as Riot from 'riot';
 
 function extend(d, element) {
-    var map = Object.keys(element.prototype).reduce(function (descriptors, key) {
+    var map = Object.keys(element.prototype).reduce((descriptors, key) => {
         descriptors[key] = Object.getOwnPropertyDescriptor(element.prototype, key);
         return descriptors;
     }, {});
     Object.defineProperties(d, map);
 }
 
-function registerService(element, service) {
-    element.prototype.load = function () {
-       // this.mixin(service);
 
-        
-        var actions = service.actions;
-        var selector = service.selector;
-        var store = this.store;
-        var isContainer = service.isContainer;
-        service.ensureSubscription(store);
-         var _this = this;
-        if(service.isContainer){
-            store.subscribe( function(){
-                _this.update();
-            });
-        }
-        this.on('mount', function () {           
-            if (service.selectorData !== undefined) {
-                Object.keys(service.selectorData).forEach(function (key) {
-                    _this[key] = service.selectorData[key];
-                });
-            }
-            if (actions !== undefined) {
-                Object.keys(actions).forEach(function (actionName) {
-                    _this[actionName] = function () {
-                        var args = [];
-                        for (var _i = 0; _i < arguments.length; _i++) {
-                            args[_i - 0] = arguments[_i];
-                        }
-                        if (window.Event && window.Event.prototype.isPrototypeOf(args[0])) {
-                            args[0].preventUpdate = true;
-                        }
-                        return store.dispatch(actions[actionName].apply(actions, args));
-                    };
-                });
-            }
-            _this.on('update', function () {                
-                if (service.selectorData !== undefined) {
-                    Object.keys(service.selectorData).forEach(function (key) {
-                        _this[key] = service.selectorData[key];
-                        if (typeof service.selectorData[key] == 'function') _this.update();
-                    });
-                }
-                if (isContainer) _this.update();
-            });
-            _this.update();
-        });
-    };
+export function registerService(element, service) {
+    element.prototype.load = function () {
+        this.mixin(service);
+    }
 }
 
-function registerElement(element, template, precompiledTags) {
+export function registerElement(element, template, precompiledTags) {
 
     element.prototype["template"] = template;
     function registerTag(compiledTag) {
 
-        var transformFunction = function transformFunction(opts) {
+        var transformFunction = function (opts) {
             extend(this, element);
             element.apply(this, [opts]);
 
@@ -85,6 +30,7 @@ function registerElement(element, template, precompiledTags) {
             if (element.prototype.beforeUnmounted !== undefined) this.on("before-unmount", this.beforeUnmounted);
             if (element.prototype.updating !== undefined) this.on("update", this.updating);
             if (element.prototype.updated !== undefined) this.on("updated", this.updated);
+
         };
 
         Riot.tag2(compiledTag.tagName, compiledTag.html, compiledTag.css, compiledTag.attribs, transformFunction, Riot.settings.brackets);
@@ -95,7 +41,7 @@ function registerElement(element, template, precompiledTags) {
     var compiled;
 
     if (element.prototype.template !== undefined) {
-        var tagTemplate = element.prototype.template;
+        let tagTemplate = element.prototype.template;
         if (precompiledTags[tagTemplate] !== undefined) {
             compiled = precompiledTags[tagTemplate];
         } else {
